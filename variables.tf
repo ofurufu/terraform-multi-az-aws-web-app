027f9e2e949cb691f65a10669d0b812602e38e86
ofurufu <ayodeleadewuyia+github@gmail.com>
2026-04-28 10:38:41 +0100

    sorted variable values in the root module

--- variables.tf
+++ variables.tf
@@ -1,3 +1,5 @@
+# ---- Global -----
+
 variable "aws_region" {
   type = string
 }
@@ -20,4 +22,99 @@ variable "environment" {
 variable "owner" {
   description = "Owner name for tagging (e.g. your name or team)"
   type        = string
+}
+
+# ---- Networking ----
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "availability_zones" {
+  description = "List of AZs to deploy into (minimum 2 for Multi-AZ)"
+  type        = list(string)
+  default     = ["us-east-1a", "us-east-1b"]
+}
+
+variable "public_subnet_cidrs" {
+  description = "CIDRs for public subnets (one per AZ)"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "private_subnet_cidrs" {
+  description = "CIDRs for private subnets (one per AZ)"
+  type        = list(string)
+  default     = ["10.0.11.0/24", "10.0.12.0/24"]
+}
+
+variable "db_subnet_cidrs" {
+  description = "CIDRs for DB subnets (one per AZ)"
+  type        = list(string)
+  default     = ["10.0.21.0/24", "10.0.22.0/24"]
+}
+
+# ---- Compute ----
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t3.micro"
+}
+
+variable "asg_min_size" {
+  description = "Minimum number of EC2 instances in the ASG"
+  type        = number
+  default     = 2
+}
+
+variable "asg_max_size" {
+  description = "Maximum number of EC2 instances in the ASG"
+  type        = number
+  default     = 4
+}
+
+variable "asg_desired_capacity" {
+  description = "Desired number of EC2 instances in the ASG"
+  type        = number
+  default     = 2
+}
+
+# ---- Database -----
+
+variable "db_name" {
+  description = "Name of the RDS database"
+  type        = string
+  default     = "appdb"
+}
+
+variable "db_username" {
+  description = "Master username for RDS"
+  type        = string
+  sensitive   = true
+}
+
+variable "db_password" {
+  description = "Master password for RDS (min 8 chars)"
+  type        = string
+  sensitive   = true
+
+  validation {
+    condition     = length(var.db_password) >= 8
+    error_message = "db_password must be at least 8 characters."
+  }
+}
+
+variable "db_instance_class" {
+  description = "RDS instance class"
+  type        = string
+  default     = "db.t3.micro"
+}
+
+variable "db_engine_version" {
+  description = "PostgreSQL engine version"
+  type        = string
+  default     = "15.4"
 }
