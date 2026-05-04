4cdbe94a2e7a899ccacddd1786d765ff7cf6ee35
ofurufu <ayodeleadewuyia+github@gmail.com>
2026-05-04 21:22:55 +0100

    created networking module.

--- versions.tf
+++ versions.tf
@@ -1,10 +1,12 @@
-terraform
+terraform {
   required_providers {
-  aws = {
-    source  = "hashicorp/aws"
-    version = "6.42.0"
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.42.0"
+    }
   }
 }
+
 provider "aws" {
   region = var.aws_region
 
@@ -16,5 +18,4 @@ provider "aws" {
       Owner       = var.owner
     }
   }
-
 }
