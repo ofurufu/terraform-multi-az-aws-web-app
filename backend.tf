3e3236f1837a2c5461088357d9b3f91145acb0ab
ofurufu <ayodeleadewuyia+github@gmail.com>
2026-05-07 20:21:50 +0100

    temporary backend remote deactivation

--- backend.tf
+++ backend.tf
@@ -1,8 +1,6 @@
+# backend.tf
 terraform {
-  backend "s3" {
-    bucket       = "tf-state-webapp-501883958979"
-    key          = "webapp/terraform.tfstate"
-    region       = "us-east-1"
-    encrypt      = true
+  backend "local" {
+    path = "terraform.tfstate"
   }
 }
